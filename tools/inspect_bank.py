e4df18943d3ea67ac479894f91f080feb2032684
Sang Huỳnh <sanh7181@gmail.com>
2026-09-04 13:05:38 +0700

    chore: remove UI_DESIGN_SPEC.md and temporary scratch files from tools

--- tools/inspect_bank.py
+++
@@ -1,9 +0,0 @@
-import json
-
-bank = json.load(open(r'd:\BaiTapCode\WHMX\NeoArtifacts\MasterData\json\talentBankMap.json', encoding='utf-8'))
-out = []
-for k, v in list(bank.items())[:5]:
-    out.append(f"{k}: icon={v.get('icon')}, name={v.get('namelanText')}")
-    
-with open(r'd:\BaiTapCode\WHMX\WhmxCalc\tools\inspect_bank_out.txt', 'w', encoding='utf-8') as f:
-    f.write('\n'.join(out))
